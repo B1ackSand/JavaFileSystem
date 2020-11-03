29c0aac65c246165ef339faeab609fc7cc0b734f
B1ackSand <peilanfengfeng@gmail.com>
2020-11-04 00:47:35 +0800

    OS 课设使用

---
+++ src/view/FrameMain.java
@@ -0,0 +1,9 @@
+package view;
+
+import javax.swing.*;
+
+public class FrameMain extends JFrame {
+    public FrameMain(){
+        System.out.println("TEXT");
+    }
+}
---
+++ src/view/RunMain.java
@@ -0,0 +1,8 @@
+package view;
+
+public class RunMain {
+    public static void main(String[] args) {
+        new FrameMain();
+    }
+
+}
